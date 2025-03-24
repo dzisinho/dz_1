909541582b23c12c4a1314fa1a178dfbfc921726
dzisinho <dzisbogdan26@gmail.com>
2025-03-24 10:07:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number (1-100): ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            Console.WriteLine(FizzBuzzChecker.GetFizzBuzz(number));
+        }
+        else
+        {
+            Console.WriteLine("Invalid input.");
+        }
+    }
+}
+
+public class FizzBuzzChecker
+{
+    public static string GetFizzBuzz(int number)
+    {
+        if (number < 1 || number > 100)
+            return "Error: Number out of range";
+
+        if (number % 3 == 0 && number % 5 == 0)
+            return "Fizz Buzz";
+        if (number % 3 == 0)
+            return "Fizz";
+        if (number % 5 == 0)
+            return "Buzz";
+
+        return number.ToString();
+    }
+}
